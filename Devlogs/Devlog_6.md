5f3290db8b8972c83f90be80aaa7fe5bfb6bd490
thamata <prilepakirill@gmail.com>
2024-01-07 15:00:28 +0100

    1.6

--- Devlogs/Devlog_6.md
+++ Devlogs/Devlog_6.md
@@ -1,4 +1,4 @@
-### Date: 
+### Date: 3/1/2024 - 7/1/2024
 
 ### Changing motion detection function
 research: 
@@ -11,6 +11,38 @@ https://pyimagesearch.com/2015/05/25/basic-motion-detection-and-tracking-with-py
 #### Thoughts
 On first glance this motion detection technique looks similar to what i am currently doing in my script, taking a frame and converting it to grayscale, then comparing it to an imput frame. Hovever, here we have a static picture that is constantly being compared to new frames from the camera feed. In comparison i dont have a static picture and am constantly comparing between just two consecutive frames, hence why my threshold is so low. I think this technique(Background subtraction) will help in making my script more accurate. 
 <br>
-I still want to keep the core functionality of my script where i utilize two cores and have an output to a GUI window, but i will see if that will be possible while i try to implement this functionallity. I may as well add a box in the video feed of what gets detected. 
+I still want to keep the core functionality of my script where i utilize two cores and have an output to a GUI window, but i will see if that will be possible while i try to implement this functionallity. I may as well add a box in the video feed of what gets detected if possible.
 
 ### Results
+In the process of improving the motion detection function i added more functionalities and imporovements. 
+
+1. Config file. The script now has a config file for fine tuning the motion detection parameters and duration of the video capture as well as the path for the recordings
+<br>
+The config file utilizes the built in python library configparser.
+2. Threading is now handled more carefully and each thread is executed and instantiated in different dunctions. I have also added a stop function for the threads.
+<br>
+Each thread still starts in _init_ but has its own function. A stop_threads function is also called upon program close that uses a flag in each threaded function. 
+<br>
+Previously if was just while(True) and now it is while self.is_running
+3. The script now stops on exiting the tkinter window.
+<br>
+This is handled by using a function in the tkinter library called protocol that then calls my stop_threads function and closes the tkinter root. 
+```Python
+    def on_closing():
+        video_stream_widget.stop_threads()  # Call the method to stop threads and release resources
+        root.destroy()  # Close the Tkinter window
+
+    # Bind the on_closing function to the window's close event
+    root.protocol("WM_DELETE_WINDOW", on_closing)
+```
+
+#### Better motion detection
+The motion detection now uses background subtraction with a built in function from openCV called BackgroundSubtractorMOG2, witch is a Gaussian Mixture-based Background/Foreground Segmentation Algorithm. 
+<br>
+I have found this method more reliable than the one i used before and it can be fine tuned using different parameters. 
+1. History: The number of frames used to build the background model
+2. varThreshold: Returns the variance threshold for the pixel-model match.
+3. detectShadows: Whether or not shadows are detected
+4. Amount of contours required to trigger as motion detected
+
+The function now only takes one frame and the Background Subtractor class as parameters, this removed the need for the fix i created regarding double recordings. However, the recorded footage is now a slight bit sped up and i have not found the cause for it yet. It is not a frame rate mismatch and it has nothing to do with the video codec. My guess is that it has to do with thread execution speeds so i will leave it as is for the time being. 
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-import time, cv2
+import time, cv2, configparser
 from tkinter import *
 from threading import Thread
 from PIL import Image, ImageTk 
+
+# Read configuration from config.ini file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Retrieve values from the configuration file
+filepath = config.get('General', 'filepath')
+min_thresh = config.getint('General', 'min_thresh')
+bgs_history = config.getint('General', 'bgs_history')
+recording_duration = config.getint('General', 'recording_duration')
 
 root = Tk()
 
@@ -12,66 +22,72 @@ label_text.pack(pady=10)
 label_video.pack(padx=50,pady=20)
 label_text2.pack(pady=10)
 
-def detect_motion(frame1, frame2):
-    # Convert frames to grayscale
-    gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+def detect_motion(frame, background_subtractor):
+    # Apply the background subtractor to get the foreground mask
+    fg_mask = background_subtractor.apply(frame)
 
-    # Compute the absolute difference between frames
-    frame_diff = cv2.absdiff(gray1, gray2)
+    # Remove noise by applying a series of morphological operations
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    fg_mask = cv2.morphologyEx(fg_mask, cv2.MORPH_OPEN, kernel)
+    fg_mask = cv2.morphologyEx(fg_mask, cv2.MORPH_CLOSE, kernel)
 
-    # Threshold the difference image
-    _, thresh = cv2.threshold(frame_diff, 180, 255, cv2.THRESH_BINARY)
-
-    # Find contours
-    contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Find contours on the foreground mask
+    contours, _ = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Check if any contour is detected
-    return len(contours) > 0
+    for c in contours:
+        if cv2.contourArea(c) > min_thresh: # Sensitivity adjustment
+            return True
 
+    return False
+
+# Class to handle video streaming, motion detection, and recording
 class VideoStreamWidget(object):
     def __init__(self, src=0):
+        # Flag to check if the threads should keep running
+        self.is_running = True
+        # Instantiate video capture
         self.capture = cv2.VideoCapture(src)
-        # Start thread to read frames from video stream
+        # Instantiate subtractor model
+        self.bsm = cv2.createBackgroundSubtractorMOG2(history= bgs_history, varThreshold=50, detectShadows=False)
+
+        # Start threads
+        self.start_update_thread()
+        self.start_show_frame_thread()
+
+    def start_update_thread(self):
         self.thread = Thread(target=self.update, args=())
         self.thread.daemon = True
         self.thread.start()
 
-        # Start thread to display frames form video feed
+    def start_show_frame_thread(self):
         self.thread2 = Thread(target=self.show_frame, args=())
         self.thread2.daemon = True
         self.thread2.start()
 
-    def update(self):
-        frame1 = None
-        frame2 = None
+    def stop_threads(self):
+        self.is_running = False  # Set the flag to False to stop the threads
+        self.capture.release()   # Release the video capture device if it's not already released
 
-        while(True):
+
+    def update(self):
+        while self.is_running:
             label_text.configure(text="Camera Online")
             ret, frame = self.capture.read()
             if not ret:
                 break
 
-            # Set frame1 to be first frame in sequence
-            if frame1 is None:
-                frame1 = frame.copy()
-                continue
-
-            # Set frame2 as previous frame1 and frame1 is set to current frame
-            frame2 = frame1
-            frame1 = frame.copy()
-
-            if detect_motion(frame1, frame2):
+            if detect_motion(frame, self.bsm):
                 t = time.time()
                 timestamp = time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime(t))
-                filename = f"C://Users//lik//Documents//Programmering//Camera_Project//Recordings//activity_{timestamp}.avi"
+                filename = f"{filepath}activity_{timestamp}.avi"
                 # print("Activity detected!")
                 
                 # Create video writer object
-                fourcc = cv2.VideoWriter_fourcc(*'XVID')
-                out = cv2.VideoWriter(filename, fourcc, 30.0, (frame.shape[1], frame.shape[0]))
+                fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+                out = cv2.VideoWriter(filename, fourcc, 30, (frame.shape[1], frame.shape[0]))
                 
-                for _ in range(150): 
+                for _ in range(recording_duration * 30): 
                     ret, frame = self.capture.read()
                     if not ret:
                         break
@@ -79,39 +95,44 @@ class VideoStreamWidget(object):
                     label_text.configure(text="Recording")
                 
                 out.release()
-                frame1 = None
                 label_text2.configure(text=f"Video saved as {filename}")
                 # print(f"Video saved as {filename}")
 
     
     def show_frame(self):
-        while(True):
+        while self.is_running:
             if self.capture.isOpened():
                 (self.status, self.frame) = self.capture.read()
 
             self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
             self.frame = Image.fromarray(self.frame)
             self.frame = ImageTk.PhotoImage(self.frame)
+
             label_video.configure(image=self.frame)
             label_video.image = self.frame
 
-            '''
-            # Display frames in main program
-            cv2.imshow('frame', self.frame)
-            key = cv2.waitKey(1)
-            if key == ord('q'):
-                self.capture.release()
-                cv2.destroyAllWindows()
-            '''
+    def on_closing():
+        video_stream_widget.stop_threads()  # Call the method to stop threads and release resources
+        root.destroy()  # Close the Tkinter window
+
+    # Bind the on_closing function to the window's close event
+    root.protocol("WM_DELETE_WINDOW", on_closing)
 
 def main():
+    # Create an instance of VideoStreamWidget
+    video_stream_widget = VideoStreamWidget()
+
     try:
-        while True:
-            root.mainloop()
+        # Start the Tkinter event loop
+        root.mainloop()
     except KeyboardInterrupt:
-        pass
+        # If KeyboardInterrupt (e.g., Ctrl+C) is detected, stop the threads gracefully
+        video_stream_widget.stop_threads()
         
 
 if __name__ == "__main__":
     video_stream_widget = VideoStreamWidget()
-    main()
+    try:
+        root.mainloop()
+    except KeyboardInterrupt:
+        video_stream_widget.stop_threads()
